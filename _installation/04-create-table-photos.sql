b45ad01aeb7f4e86e23ef4067a076e50829eacf0
Xavier Chopin <xchopin54@gmail.com>
2016-10-12 11:28:30 +0200

    Update 04-create-table-photos.sql

--- _installation/04-create-table-photos.sql
+++ _installation/04-create-table-photos.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS 'photos' (
  'photo_id' int(11) NOT NULL AUTO_INCREMENT,
  'user_id' int(11) NOT NULL,
  'description' TEXT,
+ 'url' TEXT,
  'created_at' timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
  'updated_at' timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
  PRIMARY KEY ('photos_id'),
