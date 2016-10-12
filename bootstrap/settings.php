87430f2a57e374e93b52fa972f7e3bf6a5342a0a
Alexis Wurth <alexis.wurth57@gmail.com>
2016-10-12 10:43:35 +0200

    Put database configuration in ini file

--- bootstrap/settings.php
+++ bootstrap/settings.php
@@ -17,15 +17,6 @@ return [
         ],
 
         // Database
-        'db' => [
-            'driver' => 'sqlite', // mysql or sqlite
-            'host' => 'localhost', // To change port, just add it afterwards like localhost:8889
-            'database' => 'resources/database.sqlite', // DB name or SQLite path
-            'username' => 'slim',
-            'password' => 'CZvH5nrpjJtTzcAq',
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_general_ci',
-            'prefix' => '',
-        ]
+        'db' => parse_ini_file(__DIR__ . '/db.config.ini')
     ],
 ];
