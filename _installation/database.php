06a1687db8b1b13fded56e336729f1ae6f0caffa
Alexis Wurth <alexis.wurth57@gmail.com>
2016-10-19 08:36:58 +0200

    Updated table creation script

--- _installation/database.php
+++ _installation/database.php
@@ -22,23 +22,19 @@ Manager::schema()->create('users', function (Blueprint $table) {
     $table->string('user_slug', 64)->unique();
     $table->string('user_password_hash');
     $table->string('user_email')->unique();
-    $table->boolean('user_active');
-    $table->boolean('user_deleted');
+    $table->boolean('user_active')->default(0);
+    $table->boolean('user_deleted')->default(0);
     $table->boolean('user_account_type')->default(1);
-    $table->boolean('user_has_avatar');
+    $table->boolean('user_has_avatar')->default(0);
     $table->string('user_remember_me_token', 64)->nullable();
     $table->bigInteger('user_suspension_timestamp')->nullable();
     $table->bigInteger('user_last_login_timestamp')->nullable();
-    $table->boolean('user_failed_logins');
+    $table->boolean('user_failed_logins')->default(0);
     $table->integer('user_last_failed_login')->nullable();
     $table->string('user_activation_hash')->nullable();
     $table->boolean('user_profile')->default(1);
     $table->string('user_password_reset_hash')->nullable();
     $table->bigInteger('user_password_reset_timestamp')->nullable();
-
-
-    $table->string('name');
-    $table->string('description');
     $table->timestamps();
 });
 
