4c4e9fd1a4cf33e47f2451601c968687b59421ce
Alexis Wurth <alexis.wurth57@gmail.com>
2016-11-04 15:08:02 +0100

    Added route for posting  comment

--- app/routes.php
+++ app/routes.php
@@ -36,6 +36,8 @@ $app->group('', function () {
     $this->post('/liker', 'PictureController:likeDispatcher')->setName('photo.like');
     $this->get('/user/{slug}/follow', 'SocialController:follow')->setName('user.follow');
     $this->get('/user/{slug}/unfollow', 'SocialController:unfollow')->setName('user.unfollow');
+
+    $this->post('/pic/{id}/comment', 'SocialController:postComment')->setName('comment');
 })->add(new AuthMiddleware($container));
 
 $app->group('', function () {
