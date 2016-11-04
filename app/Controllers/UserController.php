96abe1784e169e986ef9e6c3f2a2bc30275f1d0e
Alexis Wurth <alexis.wurth57@gmail.com>
2016-11-04 11:01:42 +0100

    Test if query is empty

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -3,11 +3,16 @@
 namespace App\Controllers;
 
 use App\Models\User;
+use Slim\Exception\NotFoundException;
 
 class UserController extends Controller
 {
     public function search($request, $response)
     {
+        if (!$request->getParam('q')) {
+            throw new NotFoundException($request, $response);
+        }
+
         $users = User::where('user_name', 'like', '%' . $request->getParam('q') . '%')->get();
 
         return $this->view->render($response, 'user/search.twig', [
