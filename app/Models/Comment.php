c29d97c50203c7044b3580e5669efde7eb4e5bd9
Alexis Wurth <alexis.wurth57@gmail.com>
2016-11-04 11:51:55 +0100

    Added methods for comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,4 +9,14 @@ class Comment extends Model
     protected $table = 'comment';
 
     protected $primaryKey = 'id';
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function picture()
+    {
+        return $this->belongsTo('App\Models\Picture');
+    }
 }
