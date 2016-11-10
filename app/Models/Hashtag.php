aa6515d034dcbe510e69620947950c7618b8bb4f
Alexis Wurth <alexis.wurth57@gmail.com>
2016-11-10 14:12:15 +0100

    Added method to save hashtags easily

--- app/Models/Hashtag.php
+++ app/Models/Hashtag.php
@@ -11,8 +11,75 @@ class Hashtag extends Model
 
     protected $primaryKey = 'id';
 
+    public static function saveHashtags(Picture $picture, array $tags)
+    {
+        // Parse tags in caption saved in database
+        $oldCaptionTags = array();
+        preg_match_all('/#(\w+)/', $picture->description, $oldCaptionTags);
+
+        // Difference between old and new caption
+        $removedTags = array_diff($oldCaptionTags[1], $tags);
+        $addedTags = array_diff($tags, $oldCaptionTags[1]);
+
+        $detach = array();
+        $increments = array();
+        $decrements = array();
+
+        $existingTags = $picture->hashtags;
+        foreach ($existingTags as $tag) {
+            // If the tags saved in database contain a tag that is not in the new caption
+            // and it is used only once
+            if (in_array($tag->name, $removedTags)) {
+                if ($tag->pivot->count <= 1) {
+                    $detach[] = $tag->id;
+                } else {
+                    $decrements[] = [
+                        'id' => $tag->id,
+                        'count' => $tag->pivot->count
+                    ];
+                }
+            }
+
+            if (in_array($tag->name, $addedTags)) {
+                $increments[] = [
+                    'id' => $tag->id,
+                    'count' => $tag->pivot->count
+                ];
+            }
+        }
+
+        // Add the new tags in database and attach them to the picture
+        foreach ($addedTags as $tag) {
+            $hashtag = Hashtag::where('name', $tag)->first();
+            if (!$hashtag) {
+                $hashtag = new Hashtag();
+                $hashtag->name = $tag;
+                $hashtag->save();
+            }
+
+            $picture->hashtags()->attach($hashtag->id, ['count' => 1]);
+        }
+
+        // Detach removed tags from the picture
+        if (!empty($detach)) {
+            $picture->hashtags()->detach($detach);
+        }
+
+        foreach ($increments as $increment) {
+            $picture->hashtags()->updateExistingPivot($increment['id'], [
+                'count' => $increment['count'] + 1
+            ]);
+        }
+
+        foreach ($decrements as $decrement) {
+            $picture->hashtags()->updateExistingPivot($decrement['id'], [
+                'count' => $decrement['count'] + 1
+            ]);
+        }
+    }
+
     public function pictures()
     {
-        return $this->belongsToMany('App\Models\Picture');
+        return $this->belongsToMany('App\Models\Picture')->withPivot('count');
     }
 }
