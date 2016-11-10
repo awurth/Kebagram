a1ece6f69445a64a89d1fb764b20229644f53ba3
Alexis Wurth <alexis.wurth57@gmail.com>
2016-11-10 17:17:03 +0100

    Added helper methods and PHPDoc

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -4,10 +4,12 @@ namespace App\Controllers;
 
 use App\Auth\Auth;
 use App\Validation\Validator;
+use Slim\Exception\NotFoundException;
 use Slim\Flash\Messages;
 use Slim\Router;
 use Slim\Views\Twig;
 use Interop\Container\ContainerInterface;
+use Psr\Http\Message\RequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
 /**
@@ -19,6 +21,11 @@ use Psr\Http\Message\ResponseInterface as Response;
  */
 class Controller
 {
+    /**
+     * Slim application container
+     *
+     * @var ContainerInterface
+     */
     protected $container;
 
     public function __construct(ContainerInterface $container)
@@ -26,9 +33,41 @@ class Controller
         $this->container = $container;
     }
 
+    /**
+     * Redirect to route with params
+     *
+     * @param Response $response
+     * @param string $route
+     * @param array $params
+     * @return Response
+     */
     public function redirect(Response $response, $route, array $params = array())
     {
         return $response->withRedirect($this->router->pathFor($route, $params));
+    }
+
+    /**
+     * Redirect to url
+     *
+     * @param Response $response
+     * @param string $url
+     * @return Response
+     */
+    public function redirectTo(Response $response, $url)
+    {
+        return $response->withRedirect($url);
+    }
+
+    /**
+     * Create new NotFoundException
+     *
+     * @param Request $request
+     * @param Response $response
+     * @return NotFoundException
+     */
+    public function notFoundException(Request $request, Response $response)
+    {
+        return new NotFoundException($request, $response);
     }
 
     public function __get($property)
